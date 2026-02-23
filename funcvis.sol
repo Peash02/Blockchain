59156a29942c35d86486f93d3cfbe244a9155aaa
Peash <107058247+Peash02@users.noreply.github.com>
2026-02-23 10:47:01 +0530

    Add files via upload

---
+++ funcvis.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract VisibilityDemo
+{
+    function publicFunc() public pure returns (string memory)
+    {
+        return "Public";
+    }
+    function externalFunc() external pure returns (string memory)
+    {
+        return "External";
+    }
+    function internalFunc() internal  pure returns (string memory)
+    {
+        return "Internal";
+    }function privateFunc() private pure returns (string memory)
+    {
+        return "Private";
+    }
+    function testCalls() public pure returns(string memory,string memory,string memory)
+    {
+        return(
+            publicFunc(), 
+            privateFunc(), 
+            internalFunc()
+            //externalFunc() 
+        );
+    }
+}
+contract child is VisibilityDemo
+{
+    function testChild() public pure returns (string memory,string memory)
+    {
+        return(
+            publicFunc(),
+            internalFunc()
+            //privateFunc(),
+            //externalFunc()
+        );
+    }
+
+}
